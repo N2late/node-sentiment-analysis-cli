2b429d1d372d7752a934b921e0630a5a87483670
Tiago Serra <35219940+N2late@users.noreply.github.com>
2022-09-09 09:31:52 +0200

    Implement sentiment analysis cli

---
+++ index.js
@@ -0,0 +1,54 @@
+import * as fs from 'node:fs';
+import fetch from 'node-fetch';
+
+const url = 'http://text-processing.com/api/sentiment/';
+
+async function postData(link = '', data = '') {
+  const response = await fetch(link, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: new URLSearchParams({
+      text: data,
+    }),
+  });
+  return response.json();
+}
+
+const labelExt = (label) => {
+  if (label === 'neg') {
+    return 'negative';
+  }
+  if (label === 'pos') {
+    return 'positive';
+  } else {
+    return 'neutral';
+  }
+};
+
+if (process.argv[2].includes('.txt')) {
+  const readMe = fs.readFileSync(process.argv[2], 'utf-8');
+  await postData(url, readMe).then((res) => {
+    console.log(
+      `Your text has the following sentiment:\n\n${labelExt(res.label)} with ${(
+        res.probability[res.label] * 100
+      ).toFixed(2)}% probability.`,
+    );
+  });
+} else {
+  const text = process.argv.splice(2).join(' ');
+  await postData(url, text).then((res) => {
+    console.log(
+      `Your text has the following sentiment:\n\n${labelExt(res.label)} with ${(
+        res.probability[res.label] * 100
+      ).toFixed(2)}% probability.`,
+    );
+  });
+}
